e1c04a5613d2d4f908a9a202b2afcebefaab5307
YutoKatsuno <104402349+YutoKatsuno@users.noreply.github.com>
2022-10-12 17:19:25 +0900

    Get information from website.

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ CHROME_DRIVER_PATH = os.environ["CHROME_DRIVER_PATH"]
 EMAIL = os.environ["EMAIL"]
 PASSWORD = os.environ["PASSWORD"]
 
-
 bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
 bot.get_internet_speed()
 bot.tweet_at_provider()
+
+download = bot.down
+upload = bot.up
+print(f'down: {download}')
+print(f'up: {upload}')
